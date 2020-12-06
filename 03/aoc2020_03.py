c89b8d75892f3afd18c1dc47c936a1c92bb91430
Bjoern Winkler <bjrnwnklr@gmail.com>
2020-12-06 15:38:33 +0100

    Day 03 pt 2 done

--- 03/aoc2020_03.py
+++ 03/aoc2020_03.py
@@ -1,4 +1,16 @@
 
+def traverse(slope):
+    pos = (0, 0)
+    trees = 0
+
+    while pos[0] < height - slope[0]:
+        pos = (pos[0] + slope[0], (pos[1] + slope[1]) % width)
+        if grid[pos[0]][pos[1]] == '#':
+            trees += 1
+
+    return trees
+
+
 
 f_name = 'input.txt'
 # f_name = 'ex1.txt'
@@ -12,16 +24,17 @@ height = len(grid)
 width = len(grid[0])
 
 # (row, column)
-pos = (0, 0)
-
-trees = 0
-
-while pos[0] < height-1:
-    pos = (pos[0] + 1, (pos[1] + 3) % width)
-    if grid[pos[0]][pos[1]] == '#':
-        trees += 1
-
-print(f'Part 1: Encountered {trees} trees.')
+trees_pt1 = traverse((1, 3))
+print(f'Part 1: Encountered {trees_pt1} trees.')
 
 # Part 1: 195
 
+slopes = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
+trees_pt2 = 1
+for s in slopes:
+    print(f'Pt2, slope: {s}')
+    trees_pt2 *= traverse(s)
+
+print(f'Part 2: Encountered product: {trees_pt2}')
+
+# Part 2: 3772314000
