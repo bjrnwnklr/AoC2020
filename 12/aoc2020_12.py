73e4900fbb778e50076a8115f32c7786f79b8956
Bjoern Winkler <bjoern@bjoern-winkler.de>
2020-12-24 15:43:02 +0100

    day 12 done

--- 12/aoc2020_12.py
+++ 12/aoc2020_12.py
@@ -39,23 +39,61 @@ for action, val in instructions:
 
 print(f'Position is {pos}, facing {directions[curr_dir]}. Manhattan distance {abs(pos[0]) + abs(pos[1])}.')
 
-# Part 1: 962
+# Part 1 with complex numbers
+complex_moves = {
+    'E': complex(1, 0),
+    'S': complex(0, -1),
+    'W': complex(-1, 0),
+    'N': complex(0, 1)
+}
 
-pos = (0, 0)
-wp = (-1, 10)
-
-turns = [
-
-]
+pos = complex(0, 0)
+curr_dir = complex(1, 0)
 
 for action, val in instructions:
-    if action in direction_moves:
-        # move the waypoint
-        move_in = direction_moves[action]
-        wp = (wp[0] + move_in[0] * val, wp[1] + move_in[1] * val)
+    if action in complex_moves:
+        pos += complex_moves[action] * val
     elif action == 'F':
-        pos = (pos[0] + wp[0] * val, pos[1] + wp[1] * val)
+        pos += curr_dir * val
     else:
         i = val // 90
-        sign = 1 if action == 'R' else -1
+        sign = -1j if action == 'R' else 1j
+        for _ in range(i):
+            curr_dir *= sign
 
+print(f'Position is {pos}, facing {curr_dir}. Manhattan distance {int(abs(pos.real) + abs(pos.imag))}.')
+
+# Part 1: 962
+
+# Part 2 with complex numbers
+# complex numbers: real part is the columns (positive is right/east), imag part is the rows (positive is up/north)
+
+pos = complex(0, 0)
+# wp is 10 units east and 1 unit north
+wp = complex(10, 1)
+
+complex_moves = {
+    'E': complex(1, 0),
+    'S': complex(0, -1),
+    'W': complex(-1, 0),
+    'N': complex(0, 1)
+}
+
+for action, val in instructions:
+    if action in complex_moves:
+        # move the waypoint
+        wp += complex_moves[action] * val
+    elif action == 'F':
+        # move the ship in the direction of the waypoint
+        pos += wp * val
+    else:
+        # rotate the waypoint multiple times to get to right rotation
+        i = val // 90
+        sign = -1j if action == 'R' else 1j
+        for _ in range(i):
+            wp *= sign
+
+# print out result
+print(f'Final position of ship is {pos}. Manhattan distance: {int(abs(pos.real) + abs(pos.imag))}.')
+
+# Part 2: 56135
