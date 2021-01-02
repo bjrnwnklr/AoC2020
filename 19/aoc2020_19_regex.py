3f596d4c182cb3063d4b5f84bcac5e3f7a5a4447
Bjoern Winkler <bjoern@bjoern-winkler.de>
2021-01-02 23:23:30 +0100

    day 19 part 1 done

--- 19/aoc2020_19_regex.py
+++ 19/aoc2020_19_regex.py
@@ -1,50 +1,51 @@
 import re
-from collections import defaultdict
 
-
-def print_state():
-    print('Atomic rules:')
-    print(atomic_rules)
-    print('Final rules:')
-    print(final_rules)
-    print('Rule included in:')
-    print(rule_included_in)
-    print('Rule contains:')
-    print(rule_contains)
-
-
-f_name = 'ex1.txt'
-# f_name = 'input.txt'
-
+# f_name = 'ex1.txt'
+f_name = 'input.txt'
 
 with open(f_name, 'r') as f:
-    rules, messages = f.read().split('\n\n')
+    raw_rules, raw_messages = f.read().split('\n\n')
 
-atomic_rules = []
-rule_included_in = defaultdict(set)
-rule_contains = defaultdict(list)
-final_rules = defaultdict(str)
+# process the messages and store in a list
+messages = [x.strip() for x in raw_messages.strip().split('\n')]
 
-for rule in rules.split('\n'):
+# process the rules and save them in a dict
+rules = dict()
+for rule in raw_rules.split('\n'):
     rule_num, subrules = rule.split(':')
     rule_num = int(rule_num)
     subrules = subrules.strip()
-
-    # process subrules
-    # first find the starting rules
+    # clean up "a" and "b" by removing the quotes
     if 'a' in subrules or 'b' in subrules:
-        letter = subrules.strip('\"')
-        atomic_rules.append(rule_num)
-        final_rules[rule_num] = letter
+        subrules = re.sub('\"', '', subrules)
+    # process subrules and store in rules directory
+    rules[rule_num] = subrules
 
+# start with rule 0 and replace any occurrences of digits with their rule representation
+rule_zero = rules[0]
+# find all digits in rule 0
+digits = set(map(int, re.findall(r'(\d+)', rule_zero)))
 
-print_state()
+while digits:
+    to_replace = digits.pop()
+    rule_to_insert = rules[to_replace]
+    # if the rule is atomic (i.e. a or b, or a simple statement without any OR |),
+    # don't enclose in parentheses. Only enclose otherwise.
+    if rule_to_insert not in ['a', 'b'] and '|' in rule_to_insert:
+        rule_to_insert = f'({rule_to_insert})'
+    # construct replacement pattern to find only the number (between word boundaries)
+    # and then replace all occurrences in the rule_zero
+    replacement_pattern = r'\b(' + str(to_replace) + r')\b'
+    rule_zero = re.sub(replacement_pattern, rule_to_insert, rule_zero)
+    # recreate the digits contained in the updated rule_zero string
+    digits = set(map(int, re.findall(r'(\d+)', rule_zero)))
 
-# process all finalized (atomic) rules across all rules they are contained in
-while atomic_rules:
-    # get the first atomic rule to process
-    atomic_r = atomic_rules.pop(0)
+# now flatten the pattern by removing all blanks and adding start and end anchors
+rule_zero = r'^' + re.sub(r'\s', '', rule_zero) + r'$'
+# compile the rule, then match on each message
+comp_re = re.compile(rule_zero)
 
-    ### We need to consider:
-    # - the expressions separated with | need to be nested e.g.
-    # (aa | bb) (ab | ba) | (ab | ba) (aa | bb)
+part1 = sum(1 if comp_re.search(m) else 0 for m in messages)
+print(f'Part 1: {part1}')
+
+# Part 1: 168
