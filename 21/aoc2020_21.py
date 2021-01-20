acc879793956795a68c1a632bb4a68ba26ef833a
Bjoern Winkler <bjrnwnklr@gmail.com>
2021-01-20 12:00:11 +0100

    day 21 optimized done

--- 21/aoc2020_21.py
+++ 21/aoc2020_21.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 # f_name = 'ex1.txt'
 f_name = 'input.txt'
 
@@ -13,6 +11,7 @@ with open(f_name, 'r') as f:
         # ingredients (recipes = [{'aaa', 'bbb'}, {...}]
         ingredients = line.split(' (')[0].strip().split()
         recipes.append(set(ingredients))
+        
         all_ingredients |= set(ingredients)
         # get a list of the allergens and store a dict of allergens with the food it is
         # contained in (allergens_in_food[dairy] = {1, 2, 3...}
@@ -24,16 +23,6 @@ with open(f_name, 'r') as f:
             else:
                 possible_allergens[a] &= set(ingredients)
 
-print()
-print('Recipes:')
-print(recipes)
-print()
-print('All ingredients:')
-print(all_ingredients)
-print()
-print('Possible allergens:')
-print(possible_allergens)
-
 # Part 1: count occurence of each ingredient not a possible allergen in the recipes
 all_possible_allergens = set((x for ing_set in possible_allergens.values() for x in ing_set))
 no_allergens = all_ingredients - all_possible_allergens
@@ -43,3 +32,25 @@ for ing in no_allergens:
     part1 += sum(1 for r in recipes if ing in r)
 
 print(f'Part 1: {part1}')
+
+# Part 2
+
+final_allergens = dict()
+queue = [x for x in possible_allergens if len(possible_allergens[x]) == 1]
+
+while queue:
+    allg = queue.pop(0)
+    ing = possible_allergens[allg].pop()
+
+    final_allergens[allg] = ing
+
+    possible_allergens.pop(allg)
+    for x in possible_allergens:
+        if ing in possible_allergens[x]:
+            possible_allergens[x].remove(ing)
+
+    queue = [x for x in possible_allergens if len(possible_allergens[x]) == 1]
+
+# generate the part2 output, ingredients sorted by allergen name
+part2 = ','.join([final_allergens[x] for x in sorted(final_allergens)])
+print('Part 2:', part2)
